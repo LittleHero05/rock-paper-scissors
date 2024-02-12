b14fc1a5c80c3d2478d8a75492ceb85e58850e0c
Isabella <iasbellaporraslopez@gmail.com>
2024-02-11 21:06:00 -0500

    UI ready, missing stop at 5 rounds

--- script.js
+++ script.js
@@ -1,67 +1,80 @@
 const options = ["rock", "paper", "scissors"];
+const rock = document.querySelector('input[name="rock"]');
+const paper = document.querySelector('input[name="paper"]');
+const scissors = document.querySelector('input[name="scissors"]');
+const outText = document.getElementById("outputText");
+const pCounter = document.getElementById("pCounter");
+const cCounter = document.getElementById("cCounter");
+let playerSel = '';
+let playerCount = 0;
+let computerCount = 0;
+
+function getId() {
+    rock.addEventListener('click', function() {
+        getPlayerChoice('rock');
+        playGame();
+    });
+
+    paper.addEventListener('click', function() {
+        getPlayerChoice('paper');
+        playGame();
+    });
+
+    scissors.addEventListener('click', function() {
+        getPlayerChoice('scissors');
+        playGame();
+    });
+}
 
 function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * options.length);
     return options[randomChoice];
-  }
+}
 
-function playRound(computerChoice, playerSelection){
-
-    if (computerChoice == "rock") {
-        if (playerSelection == "paper") {
-            return "player wins";
-        } else if (playerSelection == "scissors") {
-            return "computer wins";
-        } else {
-            return "tie";
-        }
-    } else if (computerChoice == "paper") {
-        if (playerSelection == "rock") {
-            return "computer wins";
-        } else if (playerSelection == "scissors") {
-            return "player wins";
-        } else {
-            return "tie";
-        }
-    } else if (computerChoice == "scissors") {
-        if (playerSelection == "paper") {
-            return "computer wins";
-        } else if (playerSelection == "rock") {
-            return "player wins";
-        } else {
-            return "tie";
-        }
+function getPlayerChoice(id) {
+    if (id === 'rock') {
+        playerSel = 'rock';
+    } else if (id === 'paper') {
+        playerSel = 'paper';
+    } else {
+        playerSel = 'scissors';
     }
 }
 
-function game() {
-    let playerCount = 0;
-    let computerCount = 0;
-
-    for (let round = 1; round <= 5; round++) {
-        console.log(`round ${round}:`);
-
-        const playerSelection = (prompt("")).toLowerCase();
-        const computerChoice = getComputerChoice();
-
-        const result = playRound(computerChoice, playerSelection);
-        console.log(result);
-        
-        if (result === "player wins") {
-            playerCount++;
-        } else if (result === "computer wins") {
-            computerCount++;
-        }
-
-        console.log(`player score: ${playerCount}`);
-        console.log(`computer score: ${computerCount}`);
+function playRound(computerChoice, playerSelection) {
+    if (computerChoice === playerSelection) {
+        return "tie";
+    } else if (
+        (computerChoice === "rock" && playerSelection === "scissors") ||
+        (computerChoice === "paper" && playerSelection === "rock") ||
+        (computerChoice === "scissors" && playerSelection === "paper")
+    ) {
+        return "computer wins";
+    } else {
+        return "player wins";
     }
 }
 
-game();
+function playGame() {
+    const playerSelection = playerSel;
+    const computerChoice = getComputerChoice();
 
-/** 
-console.log(playerCount);
-console.log(computerCount);
-console.log(computerChoice);
-console.log(playRound(computerChoice, playerSelection));*/
+    const result = playRound(computerChoice, playerSelection);
+    console.log(result);
+
+    if (result === "player wins") {
+        playerCount++;
+        outText.textContent = "player wins";
+    } else if (result === "computer wins") {
+        computerCount++;
+        outText.textContent = "computer wins";
+    }
+
+    pCounter.textContent = playerCount;
+    cCounter.textContent = computerCount;
+
+    console.log(`player score: ${playerCount}`);
+    console.log(`computer score: ${computerCount}`);
+}
+
+getId();
